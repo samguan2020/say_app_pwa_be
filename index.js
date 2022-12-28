65f6c3f9578d2c1a5f3dd456647bc0501796952f
Xin Guan <xin.guan@gmail.com>
2022-12-28 12:38:48 -0800

    remove .env stuff

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
@@ -23,8 +22,9 @@ app.get('/', (req, res) => {
 })
 
 const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL="mongodb+srv://dbadmin:Dbadmin123@cluster0.oqvj95y.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
